99dec19cd2c792186dbfc2171171c499a7e7f1bb
Maximiliano Yacobucci <maxyacobucci@gmail.com>
2024-08-05 00:31:09 -0300

    Update performance charts

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,48 +1,74 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// @mui material components
+import React, { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
-
-// Material Dashboard 2 React components
+import axios from "axios";
 import MDBox from "components/MDBox";
-
-// Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
-import Footer from "examples/Footer";
-import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
 import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
 import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
-
-// Data
-import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
-import reportsLineChartData from "layouts/dashboard/data/reportsLineChartData";
-
-// Dashboard components
-import Projects from "layouts/dashboard/components/Projects";
 import OrdersOverview from "layouts/dashboard/components/OrdersOverview";
 
 function Dashboard() {
-  const { sales, tasks } = reportsLineChartData;
+  const [portfolioPerformance, setPortfolioPerformance] = useState({
+    labels: [],
+    datasets: { label: "", data: [] },
+  });
+  const [mervalPerformance, setMervalPerformance] = useState({
+    labels: [],
+    datasets: { label: "", data: [] },
+  });
+
+  useEffect(() => {
+    const fetchPortfolioPerformanceData = async () => {
+      try {
+        const response = await axios.get(
+          "http://127.0.0.1:5000/api/v1/asset/portfolio_performance/1",
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        const data = response.data;
+        const labels = data.map((item) => item.date);
+        const values = data.map((item) => item.performance);
+        setPortfolioPerformance({
+          labels,
+          datasets: { label: "Robo Advisor Performance", data: values },
+        });
+      } catch (error) {
+        console.error("Error fetching Robo Advisor Performance data", error);
+      }
+    };
+
+    const fetchMervalPerformanceData = async () => {
+      try {
+        const response = await axios.get(
+          "http://127.0.0.1:5000/api/v1/asset/merval_performance/1",
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        const data = response.data;
+        const labels = data.map((item) => item.date);
+        const values = data.map((item) => item.performance);
+        setMervalPerformance({ labels, datasets: { label: "Merval Performance", data: values } });
+      } catch (error) {
+        console.error("Error fetching Merval Performance data", error);
+      }
+    };
+
+    fetchPortfolioPerformanceData();
+    fetchMervalPerformanceData();
+  }, []);
 
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <MDBox py={3}>
-        <Grid container spacing={3}>
+        {/*   <Grid container spacing={3}>
           <Grid item xs={12} md={6} lg={3}>
             <MDBox mb={1.5}>
               <ComplexStatisticsCard color="dark" icon="weekend" title="Total Activos" count={3} />
@@ -92,64 +118,43 @@ function Dashboard() {
               />
             </MDBox>
           </Grid>
-        </Grid>
+        </Grid>*/}
         <MDBox mt={4.5}>
           <Grid container spacing={3}>
-            {/* <Grid item xs={12} md={6} lg={4}>
-              <MDBox mb={3}>
-                <ReportsBarChart
-                  color="info"
-                  title="website views"
-                  description="Last Campaign Performance"
-                  date="campaign sent 2 days ago"
-                  chart={reportsBarChartData}
-                />
-              </MDBox>
-            </Grid> */}
-            <Grid item xs={12} md={6} lg={4}>
+            <Grid item xs={12} md={6} lg={8}>
               <MDBox mb={3}>
                 <ReportsLineChart
                   color="success"
                   title="Rendimiento Cartera Robo Advisor"
-                  description={
-                    <>
-                      (<strong>+1.5%</strong>) en el último mes
-                    </>
-                  }
                   date="recién actualizado"
-                  chart={sales}
-                />
-              </MDBox>
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              <MDBox mb={3}>
-                <ReportsLineChart
-                  color="dark"
-                  title="Índice Merval"
-                  description={
-                    <>
-                      (<strong>+2.5%</strong>) en el último mes
-                    </>
-                  }
-                  date="recién actualizado"
-                  chart={tasks}
+                  chart={portfolioPerformance}
                 />
               </MDBox>
             </Grid>
           </Grid>
         </MDBox>
-        <MDBox>
+        <MDBox mt={6.5}>
           <Grid container spacing={3}>
-            {/* <Grid item xs={12} md={6} lg={8}>
-              <Projects />
-            </Grid> */}
+            <Grid item xs={12} md={6} lg={8}>
+              <MDBox mb={3}>
+                <ReportsLineChart
+                  color="dark"
+                  title="Rendimiento Índice Merval"
+                  date="recién actualizado"
+                  chart={mervalPerformance}
+                />
+              </MDBox>
+            </Grid>
+          </Grid>
+        </MDBox>
+        {/* <MDBox>
+          <Grid container spacing={3}>
             <Grid item xs={12} md={6} lg={4}>
               <OrdersOverview />
             </Grid>
           </Grid>
-        </MDBox>
+        </MDBox> */}
       </MDBox>
-      {/* <Footer /> */}
     </DashboardLayout>
   );
 }
